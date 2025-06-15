40bcaf965a5906a59073008994a07d5ab8f3d756
dhirajCodes543 <d4dhirajbarnwal@gmail.com>
2025-06-16 00:14:14 +0530

    Update roomGeneration.js

--- Backend/services/roomGeneration.js
+++ Backend/services/roomGeneration.js
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import Ably from 'ably'
-const rest = new Ably.Rest(process.env.ABLY_KEY); // from .env
+const rest = new Ably.Rest(process.env.ABLY_KEY); 
 
 async function generateToken(clientId) {
   const tokenDetails = await rest.auth.requestToken({
     clientId,
-    ttl: 20 * 60 * 500, // valid for 1 hour
+    ttl: 20 * 60 * 500, 
   });
   return tokenDetails.token;
 }
